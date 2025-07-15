0c9bcf0ffa7dd04c579d4c047048995db99568b4
vazgen4073 <4073mkrtchyan@mail.ru>
2025-07-15 16:30:36 +0400

    Code was developed using TDD!

--- src/prime.cpp
+++ src/prime.cpp
@@ -3,56 +3,29 @@
 #include <cmath>
 #include <vector>
 
-///\brief    This function checks if the number is prime or not
-///\param    Number which should be checked for primarity
-///\return   Returns bool  1:Number is prime, 0:Number is not prime
-bool IsNumberPrime(int number)
-{
-
-    if (number < 2) {
-        return false;
-    }
-
-    if (number == 2) {
-        return true;
-    }
-
-    if (number % 2 == 0) {
-        return false;
-    }
-
-
-    for (int i = 3; i <= number / i; i += 2) {
-        if (number % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
- 
-/// @brief  The function returns a collection of integers which 
-///         contains the prime factors of the given number. 
+/// @brief  The function returns a collection of integers which
+///         contains the prime factors of the given number.
 /// @param  n is a given number
 /// @return Returns vector of collection
 std::vector<int> primeFactorize(int n)
 {
+
     std::vector<int> result;
 
-    if (n == 0 || n == 1
-        || n == 2 || n == 3) {
+    int div = 2;
+    while (div < n ) {
+        while (n % div == 0) {
+            result.push_back(div);
+            n = n / div;
+        }
+        div++;
+    }
+
+    if (n > 1) {
+        result.push_back(n);
         return result;
     }
 
-    for (int i = 2; i <= n; ++i) {
-        if (IsNumberPrime(i)) {
-            if (n % i == 0) {
-                result.push_back(i);
-                n = n / i;
-                i = 1; 
-            } 
-        }
-    }
 
     return result;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -3,44 +3,75 @@
 #include <stdio.h>
 #include <vector>
 
-TEST(prime_factor, test3)
-{
-
-    std::vector<int> tmp_vec;
-    EXPECT_EQ(primeFactorize(0).size(), 0);
-    EXPECT_EQ(primeFactorize(1).size(), 0);
-    EXPECT_EQ(primeFactorize(2).size(), 0);
-    EXPECT_EQ(primeFactorize(3).size(), 0);
-
-    tmp_vec = primeFactorize(6);
-    EXPECT_EQ(tmp_vec[0], 2);
-    EXPECT_EQ(tmp_vec[1], 3);
-
-    tmp_vec = primeFactorize(60);
-    EXPECT_EQ(tmp_vec[0], 2);
-    EXPECT_EQ(tmp_vec[1], 2);
-    EXPECT_EQ(tmp_vec[2], 3);
-    EXPECT_EQ(tmp_vec[3], 5);
-
-    tmp_vec = primeFactorize(125);
-    EXPECT_EQ(tmp_vec[0], 5);
-    EXPECT_EQ(tmp_vec[1], 5);
-    EXPECT_EQ(tmp_vec[2], 5);
-}
-
-TEST(check_prime, test2)
-{
-
-    EXPECT_TRUE(IsNumberPrime(2));
-    EXPECT_TRUE(IsNumberPrime(3));
-    EXPECT_TRUE(IsNumberPrime(5));
-    EXPECT_TRUE(IsNumberPrime(7));
-    EXPECT_TRUE(IsNumberPrime(11));
-    EXPECT_TRUE(IsNumberPrime(83));
-    EXPECT_TRUE(IsNumberPrime(97));
-}
-
-TEST(hello, EMTY_TEST)
+TEST(PrimeFctTest, EMTY_TEST)
 {
     EXPECT_EQ(1, 1);
+}
+
+TEST(PrimeFctTest, Given2_2)
+{
+    std::vector<int> expect = { 2 };
+    EXPECT_EQ(primeFactorize(2), expect);
+}
+
+TEST(PrimeFctTest, Given3_3)
+{
+    std::vector<int> expect = { 3 };
+    EXPECT_EQ(primeFactorize(3), expect);
+}
+
+TEST(PrimeFctTest, Given4_2N2)
+{
+    std::vector<int> expect = { 2, 2 };
+
+    EXPECT_EQ(primeFactorize(4), expect);
+}
+
+TEST(PrimeFctTest, Given5_5)
+{
+    std::vector<int> expect = { 5 };
+
+    EXPECT_EQ(primeFactorize(5), expect);
+}
+
+TEST(PrimeFctTest, Given6_2N3)
+{
+    std::vector<int> expect = { 2, 3 };
+
+    EXPECT_EQ(primeFactorize(6), expect);
+}
+
+TEST(PrimeFctTest, Given7_7)
+{
+    std::vector<int> expect = { 7 };
+
+    EXPECT_EQ(primeFactorize(7), expect);
+}
+
+TEST(PrimeFctTest, Given8_24)
+{
+    std::vector<int> expect = { 2, 2, 2 };
+
+    EXPECT_EQ(primeFactorize(8), expect);
+}
+
+TEST(PrimeFctTest, Given9_33)
+{
+    std::vector<int> expect = { 3, 3 };
+
+    EXPECT_EQ(primeFactorize(9), expect);
+}
+
+TEST(PrimeFctTest, Given10_25)
+{
+    std::vector<int> expect = { 2, 5 };
+
+    EXPECT_EQ(primeFactorize(10), expect);
+}
+
+TEST(PrimeFctTest, Givenlarge_25)
+{
+    std::vector<int> expect = { 2, 2, 5, 7, 13, 17 };
+
+    EXPECT_EQ(primeFactorize(2 * 2 * 5 * 7 * 17 * 13), expect);
 }
